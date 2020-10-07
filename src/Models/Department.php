4299976e7832130094d2fea6da11b9186fc49d56
Mihai <mihai.ocneanu@gmail.com>
2020-10-07 12:24:20 +0300

    first draft

--- src/Models/Department.php
+++ src/Models/Department.php
@@ -3,11 +3,12 @@
 namespace LaravelEnso\Departments\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use LaravelEnso\Helpers\Traits\HasFactory;
 use LaravelEnso\Tables\Traits\TableCache;
 
 class Department extends Model
 {
-    use TableCache;
+    use HasFactory, TableCache;
 
     protected $guarded = ['id'];
 }
