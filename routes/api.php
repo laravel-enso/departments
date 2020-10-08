9118ea1c00d4e0a3d0fcd1ff33ebc0b3e812899b
Mihai <mihai.ocneanu@gmail.com>
2020-10-08 10:55:56 +0300

    updates routes

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,30 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use LaravelEnso\Departments\Http\Controllers\Create;
+use LaravelEnso\Departments\Http\Controllers\Destroy;
+use LaravelEnso\Departments\Http\Controllers\Edit;
+use LaravelEnso\Departments\Http\Controllers\InitTable;
+use LaravelEnso\Departments\Http\Controllers\Options;
+use LaravelEnso\Departments\Http\Controllers\Store;
+use LaravelEnso\Departments\Http\Controllers\TableData;
+use LaravelEnso\Departments\Http\Controllers\Update;
 
 Route::middleware(['api', 'auth', 'core'])
     ->namespace('LaravelEnso\Departments\Http\Controllers')
     ->prefix('api/administration/departments')
     ->as('administration.departments.')
     ->group(function () {
-        Route::get('create', 'Create')->name('create');
-        Route::post('', 'Store')->name('store');
-        Route::get('{department}/edit', 'Edit')->name('edit');
+        Route::get('create', Create::class)->name('create');
+        Route::post('', Store::class)->name('store');
+        Route::get('{department}/edit', Edit::class)->name('edit');
 
-        Route::patch('{department}', 'Update')->name('update');
+        Route::patch('{department}', Update::class)->name('update');
 
-        Route::delete('{department}', 'Destroy')->name('destroy');
+        Route::delete('{department}', Destroy::class)->name('destroy');
 
-        Route::get('initTable', 'InitTable')->name('initTable');
-        Route::get('tableData', 'TableData')->name('tableData');
+        Route::get('initTable', InitTable::class)->name('initTable');
+        Route::get('tableData', TableData::class)->name('tableData');
 
-        Route::get('options', 'Options')->name('options');
+        Route::get('options', Options::class)->name('options');
     });
